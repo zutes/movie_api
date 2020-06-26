b8f88ac382ec4bda82cd5b6eb895ad67e3dbc726
zutes <zutes3479@gmail.com>
2020-06-26 08:59:03 -0600

    Updated main-view.jsx
    Import index file.

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -1,5 +1,24 @@
+// client/src/index.jsx
 import React from 'react';
-import axios from 'axios';
+import ReactDOM from 'react-dom';
+
+import { MainView } from './components/main-view/main-view';
+
+// Import statement to indicate that we need to bundle `./index.scss`
+import './index.scss';
+
+// Main component (will eventually use all the others)
+class MyFlixApplication extends React.Component {
+  render() {
+    return <MainView/>;
+  }
+}
+
+// Find the root of our app
+const container = document.getElementsByClassName('app-container')[0];
+
+// Tell React to render our app in the root DOM element
+ReactDOM.render(React.createElement(MyFlixApplication), container);
 
 class MainView extends React.Component {
     constructor() {
@@ -16,6 +35,40 @@ class MainView extends React.Component {
     render() {
       return (
        <div className="main-view"></div>
+      );
+    }
+  }
+
+  export class MainView extends React.Component {
+
+    // One of the "hooks" available in a React Component
+    componentDidMount() {
+      axios.get('<my-api-endpoint/movies>')
+        .then(response => {
+          // Assign the result to the state
+          this.setState({
+            movies: response.data
+          });
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+    
+    render() {
+      // If the state isn't initialized, this will throw on runtime
+      // before the data is initially loaded
+      const { movies } = this.state;
+  
+      // Before the movies have been loaded
+      if (!movies) return <div className="main-view"/>;
+  
+      return (
+       <div className="main-view">
+       { movies.map(movie => (
+         <div className="movie-card" key={movie._id}>{movie.Title}</div>
+       ))}
+       </div>
       );
     }
   }
