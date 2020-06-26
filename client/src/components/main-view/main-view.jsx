321f7c110a23eeb035e33454b684b7b0d4788231
zutes <zutes3479@gmail.com>
2020-06-26 09:06:06 -0600

    Updated main-view.jsx

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -1,43 +1,7 @@
-// client/src/index.jsx
 import React from 'react';
-import ReactDOM from 'react-dom';
+import axios from 'axios';
 
-import { MainView } from './components/main-view/main-view';
 
-// Import statement to indicate that we need to bundle `./index.scss`
-import './index.scss';
-
-// Main component (will eventually use all the others)
-class MyFlixApplication extends React.Component {
-  render() {
-    return <MainView/>;
-  }
-}
-
-// Find the root of our app
-const container = document.getElementsByClassName('app-container')[0];
-
-// Tell React to render our app in the root DOM element
-ReactDOM.render(React.createElement(MyFlixApplication), container);
-
-class MainView extends React.Component {
-    constructor() {
-      // Call the superclass constructor
-      // so React can initialize it
-      super();
-  
-      // Initialize the state to an empty object so we can destructure it later
-      this.state = {};
-    }
-  
-    // This overrides the render() method of the superclass
-    // No need to call super() though, as it does nothing by default
-    render() {
-      return (
-       <div className="main-view"></div>
-      );
-    }
-  }
 
   export class MainView extends React.Component {
 
