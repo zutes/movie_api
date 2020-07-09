4d96d0de9e2df75fbabf61750981b456400a3693
zutes <zutes3479@gmail.com>
2020-07-09 11:10:55 -0600

    Created main-view
    The initial main view file created.

---
+++ client/src/components/main-view/main-view.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import axios from 'axios';
+import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
+
+export class MainView extends React.Component {
+  constructor() {
+    // Call the superclass constructor
+    // so React can initialize it
+    super();
+    // Initialize the state to an empty object so we can destructure it later
+
+    this.state = {
+      movies: null,
+      selectedMovie: null,
+    };
+  }
+// This overrides the render() method of the superclass
+  // No need to call super() though, as it does nothing by default
+
+  // One of the "hooks" available in a React Component
+  componentDidMount() {
+    axios
+      .get('https://shielded-oasis-17182.herokuapp.com/movies')
+      .then((response) => {
+        // Assign the result to the state
+        this.setState({
+          movies: response.data,
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  onMovieClick(movie) {
+    this.setState({
+      selectedMovie: movie
+    });
+  }
+
+  render() {
+    const { movies, selectedMovie } = this.state;
+
+    // Before the movies have been load
