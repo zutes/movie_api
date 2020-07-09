3cbaa3b0614cd97971e95a6a315b87064742b065
zutes <zutes3479@gmail.com>
2020-07-09 11:21:37 -0600

    Created movie-card
    The initial movie-card.jsx file created.

---
+++ client/src/components/movie-card/movie-card.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export class MovieCard extends React.Component {
+  render() {
+    // This is given to the <MovieCard/> component by the outer world
+    // which, in this case, is `MainView`, as `MainView` is what’s
+    // connected to your database via the movies endpoint of your API
+    const { movie, onClick } = this.props;
+
+    return (
+      <div onClick={() => onClick(movie)} className="movie-card">{movie.Title}</div>
+    );
+  }
+}
