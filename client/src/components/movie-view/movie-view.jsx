e45d4d009ac7e897a1acfbaea8a64c6ea0ad8cdd
zutes <zutes3479@gmail.com>
2020-07-12 07:07:22 -0600

    Updated movie-view

--- client/src/components/movie-view/movie-view.jsx
+++ client/src/components/movie-view/movie-view.jsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import Button from "react-bootstrap/Button";
+import ListGroup from 'react-bootstrap/ListGroup'
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image'
 
 export class MovieView extends React.Component {
 
@@ -14,29 +20,46 @@ export class MovieView extends React.Component {
     if (!movie) return null;
 
     return (
-      <div className="movie-view">
-        <img className="movie-poster" src={movie.ImagePath} />
-        <div className="movie-title">
-          <span className="label">Title: </span>
-          <span className="value">{movie.Title}</span>
-        </div>
-        <div className="movie-description">
-          <span className="label">Description: </span>
-          <span className="value">{movie.Description}</span>
-        </div>
+      <Col>
+        <div className="movie-view">
+          <ListGroup>
+            <ListGroup.Item>
+              <Image className="img-fluid" src={movie.ImagePath} />
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <span className="label">Title: <br></br> </span>
+              <span className="value">{movie.Title}</span>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <span className="label">Description: <br></br> </span>
+              <span className="value">{movie.Description}</span>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <span className="label">Genre: <br></br> </span>
+              <span className="value">{movie.Genre.Name}</span>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <span className="label">Director: <br></br> </span>
+              <span className="value">{movie.Director.Name}</span>
+            </ListGroup.Item>
 
-        <div className="movie-genre">
-          <span className="label">Genre: </span>
-          <span className="value">{movie.Genre.Name}</span>
+            <ListGroup.Item>
+              <Button onClick={() => onClick()} className="back-button" type="button" variant="info">Back</Button>
+            </ListGroup.Item>
+          </ListGroup>
         </div>
-        <div className="movie-director">
-          <span className="label">Director: </span>
-          <span className="value">{movie.Director.Name}</span>
-        </div>
-        <div className="movie-back" onClick={() => onClick()}>
-          <button type="button">Back</button>
-        </div>
-      </div>
+      </Col>
     );
   }
 }
+
+MovieView.propTypes = {
+  movie: PropTypes.shape({
+    //ImagePath: PropTypes.string.isRequired,
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    Genre: PropTypes.object.isRequired,
+    Director: PropTypes.object.isRequired,
+  }).isRequired,
+  onClick: PropTypes.func.isRequired
+};
