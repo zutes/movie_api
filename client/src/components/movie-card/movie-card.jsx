166a6ba1573890abed87d63f48e19d19ed8008b9
zutes <zutes3479@gmail.com>
2020-07-12 07:07:12 -0600

    Updated movie-card view

--- client/src/components/movie-card/movie-card.jsx
+++ client/src/components/movie-card/movie-card.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+import "./movie-card.scss";
 
 export class MovieCard extends React.Component {
   render() {
     const { movie, onClick } = this.props;
 
     return (
-      <div onClick={() => onClick(movie)} className="movie-card">{movie.Title}</div>
+      <Card style={{ width: '16rem' }}>
+        <Card.Img variant="top" src={movie.ImagePath} />
+        <Card.Body>
+          <Card.Title>{movie.Title}</Card.Title>
+          <Card.Text>{movie.Description}</Card.Text>
+          <Button onClick={() => onClick(movie)} variant="info">Open</Button>
+        </Card.Body>
+      </Card>
     );
   }
 }
@@ -15,7 +26,7 @@ MovieCard.propTypes = {
   movie: PropTypes.shape({
     Title: PropTypes.string.isRequired,
     Description: PropTypes.string.isRequired,
-    //ImagePath: PropTypes.string.isRequired
+    ImagePath: PropTypes.string.isRequired
   }).isRequired,
   onClick: PropTypes.func.isRequired
 };
