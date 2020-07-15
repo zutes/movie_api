580a4ce91f4b0a16aadf26ef8743b68ac54fa39b
Zach <zutes3479@gmail.com>
2020-07-15 09:39:27 -0600

    enable jwt authentication for movies endpoint

--- client/dist/src.78399e21.js
+++ client/dist/src.78399e21.js
@@ -34657,7 +34657,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53194" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53310" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- index.js
+++ index.js
@@ -41,18 +41,35 @@ mongoose.connect(process.env.CONNECTION_URI, {
 const cors = require("cors");
 
 //CORS sites granted access
-
+/*
 let allowedOrigins = [
   "http://localhost:8080",
   "http://localhost:1234",
   "http://shielded-oasis-17182.herokuapp.com/",
   "https://shielded-oasis-17182.herokuapp.com/",
 ];
+*/
 
+let allowedOrigins = [
+  '*',
+];
 
-app.use(cors());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        // If a specific origin isn’t found on the list of allowed origins
+        let message =
+          "The CORS policy for this application doesn’t allow access from origin " +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
-let auth = require("./auth")(app);
 app.get("/", (req, res) => {
   res.send("Welcome to the myFlix app!");
   console.log("Welcome to the myFlix app!");
