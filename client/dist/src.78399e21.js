ab439b39cc0fd20fed580a6a26a993747c8fb29a
Zach <zutes3479@gmail.com>
2020-07-15 08:40:05 -0600

    enable jwt authentication for movies endpoint

--- client/dist/src.78399e21.js
+++ client/dist/src.78399e21.js
@@ -34657,7 +34657,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52482" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52534" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
