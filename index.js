f68b2f09240c5cc60bf9ebd9122cae4872fd9e98
Zach <zutes3479@gmail.com>
2020-07-15 12:26:45 -0600

    enable jwt authentication for movies endpoint

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.use(
     },
   })
 );
-
+let auth = require("./auth")(app);
 app.get("/", (req, res) => {
   res.send("Welcome to the myFlix app!");
   console.log("Welcome to the myFlix app!");
