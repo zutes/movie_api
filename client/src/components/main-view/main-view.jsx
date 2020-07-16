1f014ae01e71a44205128f65de92bd1314ea83e2
Zach <zutes3479@gmail.com>
2020-07-16 15:03:15 -0600

    enable jwt authentication for movies endpoint

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -45,11 +45,33 @@ export class MainView extends React.Component {
     });
   }
 
-  onLoggedIn(user) {
+  
+onLoggedIn(authData) {
+  console.log(authData);
+  this.setState({
+    user: authData.user.Username
+  });
+
+  localStorage.setItem('token', authData.token);
+  localStorage.setItem('user', authData.user.Username);
+  this.getMovies(authData.token);
+}
+
+
+getMovies(token) {
+  axios.get('https://shielded-oasis-17182.herokuapp.com/movies', {
+    headers: { Authorization: `Bearer ${token}`}
+  })
+  .then(response => {
+    // Assign the result to the state
     this.setState({
-      user
+      movies: response.data
     });
-  }
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
 
   onRegister(register) {
     this.setState({
--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.get('/documentation.html', (req, res) => {
 //Gets a list of all movies
 app.get(
   '/movies',
-
+  passport.authenticate('jwt', { session: false }),
   (req, res) => {
     Movies.find()
       .then((movies) => {
