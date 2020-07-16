f4a492da3e60bee219f5d3e7be44d99db563ba7b
Zach <zutes3479@gmail.com>
2020-07-16 14:24:56 -0600

    enable jwt authentication for movies endpoint

--- index.js
+++ index.js
@@ -52,8 +52,23 @@ let allowedOrigins = [
 
 let allowedOrigins = ['*'];
 
-app.use(cors())
-let auth = require('./auth')(app)
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        // If a specific origin isn’t found on the list of allowed origins
+        let message =
+          'The CORS policy for this application doesn’t allow access from origin ' +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
+
+let auth = require('./auth')(app);
 
 app.get('/', (req, res) => {
   res.send('Welcome to the myFlix app!');
