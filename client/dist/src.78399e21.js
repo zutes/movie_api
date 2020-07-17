cc1257814f64b92960a35d7b270b08ccc19002e6
zutes <zutes3479@gmail.com>
2020-07-17 12:20:05 -0600

    Troubleshooting

--- client/dist/src.78399e21.js
+++ client/dist/src.78399e21.js
@@ -34652,7 +34652,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64196" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49307" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
