9fc42855b0dc2a22a5fc2e1d4c664a940f9e77ba
Zach <zutes3479@gmail.com>
2020-07-17 09:38:29 -0600

    enable jwt authentication for movies endpoint

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const { check, validationResult } = require("express-validator");
 //Authentication(passport) and Authorization(auth)
 const passport = require("passport");
 require("./passport");
-
+let auth = require("./auth")(app);
 
 //Integrating Mongoose with REST API
 const mongoose = require("mongoose");
@@ -51,8 +51,6 @@ let allowedOrigins = [
 ];
 
 app.use(cors())
-
-let auth = require("./auth")(app);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the myFlix app!");
