4ec3580ea45b5abb570943d6368fc4a66520a601
Zach <zutes3479@gmail.com>
2020-08-15 07:33:26 -0600

    enable cors for local react app/all domains

--- index.js
+++ index.js
@@ -78,20 +78,16 @@ app.get('/documentation.html', (req, res) => {
   res.sendFile('documentation.html');
 });
 
-app.get(
-  '/users/:Username',
-  passport.authenticate('jwt', { session: false }),
-  function (req, res) {
-    Users.findOne({ Username: req.params.Username })
-      .then(function (user) {
-        res.json(user);
-      })
-      .catch(function (err) {
-        console.error(err);
-        res.status(500).send('Error: ' + err);
-      });
-  }
-);
+app.get('/users/:Username', (req, res) => {
+  Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
 
 //Gets a list of all movies
 app.get(
