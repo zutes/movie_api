9d4dd98f3dd1cb99133ef6d0f7baee62558970f9
zutes <zutes3479@gmail.com>
2020-08-19 11:27:11 -0600

    Updated files

--- client/src/actions/actions.js
+++ client/src/actions/actions.js
@@ -1,3 +1,4 @@
+//SET_MOVIES initializes the movies list with movies; SET_FILTER sets the filter to filter your movies list.
 export const SET_MOVIES = 'SET_MOVIES';
 export const SET_FILTER = 'SET_FILTER';
 
